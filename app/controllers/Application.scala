b080adc2d1dce3b2ad959d73dd748ea447cb418c
Sergio S <serhii.solomakha@gmail.com>
2015-02-11 23:46:30 +0200

    Adding Play app stub project

---
+++ app/controllers/Application.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import models.Task
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    //Ok(views.html.index("Your new application is ready."))
+    Redirect(routes.Application.tasks)
+  }
+
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
+
+
+  def deleteTask(id: Long) = Action {
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
+  }
+
+
+  import play.api.data._
+  import play.api.data.Forms._
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+}
